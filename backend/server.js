58ab12aa7f1d9691ccc7d24d5bc212e4e5f1e840
Raegan Faith Paguirigan <57030395+raeganfaith@users.noreply.github.com>
2024-08-22 09:37:56 +0800

    Added .env to .gitignore and configured dotenv in server.js, added /products route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+app.get("/products", (req, res) => {
+  res.send("Server is ready!");
+});
 
 app.listen(5000, () => {
   console.log("Server started at http://localhost:5000");
