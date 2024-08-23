fa92b9fe49cde9904866f8efd49126931b44218b
Raegan Faith Paguirigan <57030395+raeganfaith@users.noreply.github.com>
2024-08-23 15:08:10 +0800

    Added db.js file with mongoose connection function

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 import express from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
+import { connectDB } from "./config/db.js";
 
 const app = express();
+
 
 app.get("/products", (req, res) => {
   res.send("Server is ready!");
 });
 
+// This function will execute if the backend runs: Enter npm run dev on backend terminal
 app.listen(5000, () => {
+   connectDB();
+  // This message log indicates that the backend is running
   console.log("Server started at http://localhost:5000");
 });
