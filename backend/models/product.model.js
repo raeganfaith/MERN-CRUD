10974b9431065986596810cd64914266ff121f17
Raegan Faith Paguirigan <57030395+raeganfaith@users.noreply.github.com>
2024-08-24 14:48:48 +0800

    Updated product model and added CRUD endpoints for products in server.js

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -1,9 +1,28 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 // Create a product schema
-const productSchema = new mongoose.Schema({
-  name:{
-    type: String,
-    required: true,
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
   },
-});
+  {
+    timestamps: true, // createdAt and updatedAt
+  }
+);
+
+// Product model, create a collection labelled as 'Product' using the productSchema
+const Product = mongoose.model("Product", productSchema);
+
+// To use this in different files
+export default Product;
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,63 @@
 import express from "express";
+import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
+
+dotenv.config();
 
 const app = express();
 
+app.use(express.json()); //allows us to accept JSON data in the req.body
 
-app.get("/products", (req, res) => {
-  res.send("Server is ready!");
+// GET ALL PRODUCTS ENDPOINT
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("error in fetching products:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+// POST ENDPOINT
+app.post("/api/products", async (req, res) => {
+  const product = req.body; // user will send this data
+
+  // check if the requirements is empty.
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+
+  // if the user pass the requirements then it can now create a new product based on the user data.
+  const newProduct = Product(product);
+
+  try {
+    await newProduct.save(); //save it to the database
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in creating the product", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+// DELETE ENDPOINT
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted" });
+  } catch (error) {
+    res.status(404).json({ success: false, message: "Product not found" });
+  }
 });
 
 // This function will execute if the backend runs: Enter npm run dev on backend terminal
 app.listen(5000, () => {
-   connectDB();
+  connectDB();
   // This message log indicates that the backend is running
   console.log("Server started at http://localhost:5000");
 });
