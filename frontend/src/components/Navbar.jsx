d89033c16b83c3531622e533d9a08d3f9b6e8797
Raegan Faith Paguirigan <57030395+raeganfaith@users.noreply.github.com>
2024-08-27 00:13:15 +0800

    Added Chakra UI components to Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,18 @@
+import { Container, Flex, Text } from "@chakra-ui/react";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{ base: "column", sm: "row" }}
+      >
+        <Text></Text>
+      </Flex>
+    </Container>
+  );
 };
 
 export default Navbar;
