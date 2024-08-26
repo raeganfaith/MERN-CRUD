e568605d77d54967a8d344b27947e7fbddb21114
Raegan Faith Paguirigan <57030395+raeganfaith@users.noreply.github.com>
2024-08-26 23:50:29 +0800

    Updated App.jsx to use Routes and Navbar, removed react.svg file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
-import { Button } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Button>Hello World</Button>
+      <Box>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/create" element={<CreatePage />}></Route>
+        </Routes>
+      </Box>
     </>
   );
 }
