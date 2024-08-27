7977ff551d86025e3723bc33ba7147b9f3894de1
Raegan Faith Paguirigan <57030395+raeganfaith@users.noreply.github.com>
2024-08-27 20:29:36 +0800

    Updated App.jsx to add background color and min height to Box component, and updated Navbar.jsx to import additional Chakra UI components and add dark/light theme toggle button

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,16 @@
-import { Button, Container, Flex, HStack, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import { PlusSquareIcon } from "@chakra-ui/react";
+import { MoonIcon, PlusSquareIcon, SunIcon } from "@chakra-ui/icons";
 
 const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode(); //chakra ui hook for dark/light theme toggle function
   return (
     <Container maxW={"1140px"} px={4}>
       <Flex
@@ -22,11 +30,15 @@ const Navbar = () => {
           <Link to={"/"}>Product Store 🛒</Link>
         </Text>
         <HStack spacing={2} alignItems={"center"}>
+          {/* Create Page Nav Button */}
           <Link to={"/create"}>
             <Button>
               <PlusSquareIcon fontSize={20} />
             </Button>
           </Link>
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+          </Button>
         </HStack>
       </Flex>
     </Container>
